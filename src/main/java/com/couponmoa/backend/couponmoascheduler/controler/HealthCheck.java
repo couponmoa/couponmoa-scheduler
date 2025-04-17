c7bc0e621d8dbbfa1156b6661e74d792fe34b167
plusmate <psejin1478@gmail.com>
2025-04-18 00:12:31 +0900

    feat(#4): healthcheck 컨트롤러 구현

---
+++ src/main/java/com/couponmoa/backend/couponmoascheduler/controler/HealthCheck.java
@@ -0,0 +1,18 @@
+package com.couponmoa.backend.couponmoascheduler.controler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheck {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
+
