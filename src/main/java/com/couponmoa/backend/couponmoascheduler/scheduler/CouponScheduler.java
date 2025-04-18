aadf501e5ac83d30605fb67c477d32ec3557f029
iboyeon0816 <iboyeon0816@gmail.com>
2025-04-18 11:21:46 +0900

    refactor: 스케줄러 패키지 분리

--- src/main/java/com/couponmoa/backend/couponmoascheduler/scheduler/CouponScheduler.java
+++ src/main/java/com/couponmoa/backend/couponmoascheduler/scheduler/CouponScheduler.java
@@ -1,4 +1,4 @@
-package com.couponmoa.backend.couponmoascheduler.common.scheduler;
+package com.couponmoa.backend.couponmoascheduler.scheduler;
 
 import com.couponmoa.backend.couponmoascheduler.domain.coupon.dto.CouponQuantityDto;
 import com.couponmoa.backend.couponmoascheduler.domain.coupon.dto.CouponIdDto;
