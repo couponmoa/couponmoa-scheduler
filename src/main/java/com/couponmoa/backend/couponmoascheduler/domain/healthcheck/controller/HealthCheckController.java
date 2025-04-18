425c0f0ddbfbfe6e304d7a921b9b9277877de4a1
iboyeon0816 <iboyeon0816@gmail.com>
2025-04-18 16:01:45 +0900

    refactor: HealthCheckController domain 패키지 내부로 이동

---
+++ src/main/java/com/couponmoa/backend/couponmoascheduler/domain/healthcheck/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.couponmoa.backend.couponmoascheduler.domain.healthcheck.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
+
