76e8f567b743c71121d849519b682bb049bf6508
iboyeon0816 <iboyeon0816@gmail.com>
2025-04-21 14:49:12 +0900

    fix: HealthCheckController 중복 코드 제거

--- src/main/java/com/couponmoa/backend/couponmoascheduler/controller/HealthCheckController.java
+++ src/main/java/com/couponmoa/backend/couponmoascheduler/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.couponmoa.backend.couponmoascheduler.domain.healthcheck.controller;
+package com.couponmoa.backend.couponmoascheduler.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
