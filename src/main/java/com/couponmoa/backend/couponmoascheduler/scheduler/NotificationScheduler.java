eca46fa39899f9a56aab0085e9905c051044a4c6
나연 <kny5579@naver.com>
2025-04-26 21:27:05 +0900

    chore(#16): 배포용 설정 파일 추가

--- src/main/java/com/couponmoa/backend/couponmoascheduler/scheduler/NotificationScheduler.java
+++ src/main/java/com/couponmoa/backend/couponmoascheduler/scheduler/NotificationScheduler.java
@@ -14,19 +14,19 @@ public class NotificationScheduler {
     private final RestTemplate restTemplate;
 
     // TODO: url 서버 명으로 수정
-    @Scheduled(cron = "0 0 0 * * *")
-    public void expireCouponNotifications() {
-        String url = "http://localhost:8080/api/v1/notifications";
-        try {
-            ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, null, Void.class);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                System.out.println("만료 전 알림 스케줄러 실행");
-            } else {
-                System.err.println("API 호출 실패: " + response.getStatusCode());
-            }
-        } catch (Exception e) {
-            System.err.println("API 호출 중 오류 발생: " + e.getMessage());
-        }
-    }
+//    @Scheduled(cron = "0 0 0 * * *")
+//    public void expireCouponNotifications() {
+//        String url = "http://localhost:8080/api/v1/notifications";
+//        try {
+//            ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, null, Void.class);
+//            if (response.getStatusCode().is2xxSuccessful()) {
+//                System.out.println("만료 전 알림 스케줄러 실행");
+//            } else {
+//                System.err.println("API 호출 실패: " + response.getStatusCode());
+//            }
+//        } catch (Exception e) {
+//            System.err.println("API 호출 중 오류 발생: " + e.getMessage());
+//        }
+//    }
 }
 
